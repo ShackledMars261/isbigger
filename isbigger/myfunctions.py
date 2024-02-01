430283f4c64fb23b4c6abd58cf50ec7e0cac3739
Jack Martin <jrm9229@gmail.com>
2024-02-01 10:50:22 -0600

    initial commit

---
+++ isbigger/myfunctions.py
@@ -0,0 +1,26 @@
+def isBigger(int1: int, int2: int) -> bool:
+    """
+    Returns True if big is greater than small, False otherwise
+    :param int1: int
+    :param int2: int
+    :return: True if int1 > int2, False otherwise
+    """
+    return int1 > int2
+
+def isSmaller(int1: int, int2: int) -> bool:
+    """
+    Returns True if small is less than big, False otherwise
+    :param int1: int
+    :param int2: int
+    :return: True if int1 < int2, False otherwise
+    """
+    return int1 < int2
+
+def isEqual(int1: int, int2: int) -> bool:
+    """
+    Returns True if int1 is equal to int2, False otherwise
+    :param int1: int
+    :param int2: int
+    :return: True if int1 == int2, False otherwise
+    """
+    return int1 == int2
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="isbigger",
+    packages=find_packages(include=["isbigger"]),
+    version="0.1",
+    description="A package to compare two integers",
+    author="Jack Martin"
+)
