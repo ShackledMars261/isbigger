aff7add59cff907cae58a81c8b192944151b9630
Jack Martin <jrm9229@gmail.com>
2024-02-01 18:19:15 -0600

    5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="is_bigger",
     packages=find_packages(include=["is_bigger"]),
-    version="0.1",
+    version="0.1.1",
     description="A package to compare two integers",
     author="ShackledMars261",
     install_requires=[],
