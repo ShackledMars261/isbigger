2240a0e5639842a106446f1e924854d4fd8b739d
Jack Martin <jrm9229@gmail.com>
2024-02-01 18:14:48 -0600

    4

---
+++ README.md
@@ -0,0 +1,9 @@
+# is_bigger
+
+```
+from is_bigger.functions import *
+
+isBigger(2,1) # True
+isSmaller(2,1) # False
+isEqual(3,3) # True
+```
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(include=["is_bigger"]),
     version="0.1",
     description="A package to compare two integers",
-    author="Jack Martin",
+    author="ShackledMars261",
     install_requires=[],
     setup_requires=["pytest-runner"],
     tests_require=["pytest==4.4.1"],
