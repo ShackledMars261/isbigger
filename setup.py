c2b581474a78d67b6b559387d282184166927003
Jack Martin <jrm9229@gmail.com>
2024-02-05 12:41:07 -0600

    commit to the bit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 setup(
     name="is_bigger",
     packages=find_packages(include=["is_bigger"]),
-    version="0.1.7",
+    version="0.1.8",
     description="A package to compare two integers",
     author="ShackledMars261",
     install_requires=[],
